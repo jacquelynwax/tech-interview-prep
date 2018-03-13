26cb13232210222d13e3e0acec2dba643c308550
Jacquelyn Wax <jacquelynwax@gmail.com>
2018-03-13 16:47:47 -0400

    commiting before changing computers

--- client/components/PowerPose.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PowerPose = props => {
-  return (
-    <p>Now take a power pose!</p>
-  )
-}
-
-export default PowerPose
--- server/api/users.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const {User} = require('../db/models')
-module.exports = router
-
-router.get('/:userId', (req, res, next) => {
-  User.findById(req.params.userId)
-    .then(user => res.json(user))
-    .catch(next)
-})
